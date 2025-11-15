9c3197cc7c6ee2ceb3ca471ef9b8db586674d013
vionethalavega20 <vionetha2085@gmail.com>
2025-11-15 18:34:57 +0700

    install & add cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import api from "./routes/api.js";
 import database from "./config/database.js"
+import cors from "cors"
+
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/api', api)
