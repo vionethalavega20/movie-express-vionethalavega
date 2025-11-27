c6f53f6c1ec9ac36b64e6587409df7caa105632b
vionethalavega20 <vionetha2085@gmail.com>
2025-11-27 11:54:25 +0700

    update movieModel.js

--- models/movieModel.js
+++ models/movieModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import UserModel from './userModel';
 
 const movieSchema = new mongoose.Schema(
     {
@@ -20,6 +21,11 @@ const movieSchema = new mongoose.Schema(
             required: true,
             trim: true
         },
+        
+        createdBy:{
+            type: mongoose.Types.ObjectId,
+            ref: UserModel
+        }
     },
     { 
         timestamps: true,
